3c1b936ef6cbdc8719bc7884620d436260fe3b55
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-20 17:46:12 -0600

    Create w/ Code
    Finished Lesson 1.2

---
+++ PracticeCode/CreateWithCode/Prototype1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+     transform.Translate(Vector3.forward*Time.deltaTime*50);   
+    }
+}
