ecc1ea0dea87b0cbbbd6df8a0c3e6b9965b71cb0
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-29 22:39:40 -0600

    Python Practice

---
+++ PracticeCode/Python/SpaceShooterTutorial/main.py
@@ -0,0 +1,56 @@
+import pygame
+import os
+import time
+import random
+pygame.font.init()
+
+WIDTH, HEIGHT = 750, 650
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+# Load images
+R_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+G_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+B_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Player ship
+Y_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Lasers
+R_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+G_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+B_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+Y_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")),(WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    clock = pygame.time.Clock()
+
+
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+main()
